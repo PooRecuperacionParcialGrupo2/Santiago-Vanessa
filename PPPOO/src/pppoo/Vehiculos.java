8bdd3989f2eb32cbafb3014a1a7c2783c2dc706a
vanessaMarti339 <vmartinezp@unal.edu.co>
2026-03-20 18:21:53 -0500

    vehiculo
    ya se organizo

--- PPPOO/src/pppoo/Vehiculos.java
+++ PPPOO/src/pppoo/Vehiculos.java
@@ -9,5 +9,45 @@ package pppoo;
  * @author vanes
  */
 public class Vehiculos {
+   private String placa;
+   private String marca;
+   private String modelo;
+   
+
+
+    public Vehiculos(String placa, String marca, String modelo){
+       this.placa=placa;
+       this.marca=marca;
+       this.modelo=modelo;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
     
+    public void mostrarInfoGeneral() {
+        System.out.println("Vehículo/ Placa: "+ placa +" Marca: " + marca );
+    }
+    
+    public abstract void calcularAutonomia();
 }
